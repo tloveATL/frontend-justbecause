827f709ac034ba3e3905568407e517a2afb1ee47
tloveATL <tlove.weaver@gmail.com>
2019-10-11 11:22:29 -0400

    additional work on signup and login

--- src/Actions/fetches.js
+++ src/Actions/fetches.js
@@ -10,8 +10,8 @@ export const userPostFetch = (user) => {
     })
         .then(resp => resp.json())
         .then(data => {
-            if (data.error) {
-                alert(data.error)
+            if (data.message) {
+                alert(data.message)
         } else {
             localStorage.setItem("token", data.jwt)
             dispatch(loginUser(data.user))
@@ -24,3 +24,53 @@ const loginUser = (userObj) => ({
     type: 'LOGIN_USER',
     payload: userObj
 })
+
+export const logoutUser = () => ({
+    type: 'LOGOUT_USER'
+})
+
+export const userLoginFetch = user => {
+    return dispatch => {
+        return fetch("http://localhost:3000/api/v1/login", {
+        method: "POST",
+        headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        },
+        body: JSON.stringify({user})
+    })
+        .then(resp => resp.json())
+        .then(data => {
+            if (data.message) {
+                alert(data.message)
+        } else {
+            localStorage.setItem("token", data.jwt)
+            dispatch(loginUser(data.user))
+        }
+        })
+    }
+}
+
+export const getProfileFetch = () => {
+    return dispatch => {
+        const token = localStorage.token;
+        if (token) {
+        return fetch("http://localhost:3000/api/v1/profile", {
+            method: "GET",
+            headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    })
+        .then(resp => resp.json())
+        .then(data => {
+            if (data.error) {
+            localStorage.removeItem("token")
+            } else {
+            dispatch(loginUser(data.user))
+            }
+        })
+    }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getProfileFetch, logoutUser } from './Actions/fetches';
+import Signup from './components/Signup';
+import Login from './components/Login';
 import './App.css';
-import Signup from './components/Signup'
 
 
-function App() {
-  return (
-    <div className="App">
-    <Signup/>
-    </div>
-  );
+class App extends Component {
+  componentDidMount = () => {
+    this.props.getProfileFetch()
+  }
+
+  handleClick = (e) => {
+    e.preventDefault()
+    localStorage.removeItem("token")
+    this.props.logoutUser()
+  }
+  
+  
+  render(){
+    return (
+      <div>
+        <Switch>
+          <Route path="/signup" component={ Signup }/>
+          <Route path="/login" component={ Login }/>
+      </Switch>
+        {this.props.currentUser.username 
+        ? <button onClick={this.handleClick}>Log Out</button> 
+        : null
+        }
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  currentUser: state.currentUser
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  getProfileFetch: () => dispatch(getProfileFetch()),
+  logoutUser: () => dispatch(logoutUser())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
